37261851f5662120e199efb44fcb1a9d10e0e171
Nitin Rastogi <c-nitin.rastogi@chartercom.com>
2020-06-07 23:36:01 -0400

    Corrected Router Path - Component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 import ToDos from "../views/ToDos.vue";
 import todo from "../components/todo.vue";
 
@@ -25,11 +26,7 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
   }
 ];
 
